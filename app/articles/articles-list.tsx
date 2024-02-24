67dc1ebebd1dee8250747a39d5e56f165f875543
Ali <ali@cigari.co.uk>
2024-02-24 20:53:58 +0000

    feat(scroll): infinite scroll to load less articles - probably has bugs

--- app/articles/articles-list.tsx
+++ app/articles/articles-list.tsx
@@ -4,12 +4,60 @@ import { Database, Article } from "../database.types";
 import {
   Session,
   createClientComponentClient,
+  SupabaseClient,
 } from "@supabase/auth-helpers-nextjs";
 import ArticleRow from "./article-row";
 
+const DEFAULT_ARTICLES_PER_PAGE = 50;
+
+let getQuery = (supabase: SupabaseClient, sort: string, filter: string) => {
+  let query = supabase.from("article").select(`*`);
+
+  const cutOff = new Date();
+  cutOff.setDate(cutOff.getDate() - (sort === "Top Score" ? 2 : 5));
+
+  if (filter === "New News") {
+    query = query.eq("archived", false).eq("read", false);
+  }
+
+  if (
+    filter !== "Down" &&
+    filter !== "Saved" &&
+    filter !== "Up" &&
+    filter !== "Archived"
+  ) {
+    query = query.gte("score", 0).gte("published_at", cutOff.toISOString());
+  }
+
+  if (filter === "Read and Unread News") {
+    query = query.eq("archived", false);
+  } else if (filter === "Saved") {
+    query = query.eq("saved", true);
+  } else if (filter === "Archived") {
+    query = query.eq("archived", true);
+  } else if (filter === "Down") {
+    query = query.lt("score", 0);
+  } else if (filter === "Up") {
+    query = query.gt("score", 0);
+  }
+
+  if (sort === "Top Score") {
+    query = query
+      .order("score", { ascending: false })
+      .order("published_at", { ascending: false });
+  } else {
+    query = query.order("published_at", { ascending: false });
+  }
+
+  return query;
+};
+
 export default function ArticleList({ session }: { session: Session | null }) {
   const supabase = createClientComponentClient<Database>();
+
   const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [initialLoadDone, setInitialLoadDone] = useState(false);
   const [allArticles, setAllArticles] = useState<Article[]>([]);
   const [articles, setArticles] = useState<Article[]>([]);
   const [allPublications, setAllPublications] = useState<string[]>([]);
@@ -67,81 +115,6 @@ export default function ArticleList({ session }: { session: Session | null }) {
 
   const user = session?.user;
 
-  const getArticles = useCallback(async () => {
-    try {
-      setLoading(true);
-
-      let query = supabase.from("article").select(`*`);
-
-      const cutOff = new Date();
-      cutOff.setDate(cutOff.getDate() - (sort === "Top Score" ? 2 : 5));
-
-      if (filter === "New News") {
-        query = query.eq("archived", false).eq("read", false);
-      }
-
-      if (
-        filter !== "Down" &&
-        filter !== "Saved" &&
-        filter !== "Up" &&
-        filter !== "Archived"
-      ) {
-        query = query.gte("score", 0).gte("published_at", cutOff.toISOString());
-      }
-
-      if (filter === "Read and Unread News") {
-        query = query.eq("archived", false);
-      } else if (filter === "Saved") {
-        query = query.eq("saved", true);
-      } else if (filter === "Archived") {
-        query = query.eq("archived", true);
-      } else if (filter === "Down") {
-        query = query.lt("score", 0);
-      } else if (filter === "Up") {
-        query = query.gt("score", 0);
-      }
-
-      if (sort === "Top Score") {
-        query = query
-          .order("score", { ascending: false })
-          .order("published_at", { ascending: false });
-      } else {
-        query = query.order("published_at", { ascending: false });
-      }
-      let { data, error, status } = await query
-        //.eq('user_id', user?.id)
-        .limit(500);
-
-      if (error && status !== 406) {
-        throw error;
-      }
-
-      if (data) {
-        // get unique publication names
-        const publications = Array.from(
-          new Set(data.map((article) => article.publication)),
-        );
-        setAllPublications(publications);
-
-        setArticles(
-          data.filter(
-            (article) =>
-              selectedPublications.length == 0 ||
-              selectedPublications.includes(article.publication),
-          ),
-        );
-        if (selectedPublications.length == 0) {
-          setSelectedPublications(publications);
-        }
-        setAllArticles(data);
-      }
-    } catch (error) {
-      alert("Error loading articles data!");
-    } finally {
-      setLoading(false);
-    }
-  }, [supabase, filter, sort, selectedPublications]);
-
   const handlePublicationChange = (event: any) => {
     const selected = Array.from(event.target.selectedOptions).map(
       (o: any) => o.value,
@@ -151,10 +124,6 @@ export default function ArticleList({ session }: { session: Session | null }) {
       allArticles.filter((article) => selected.includes(article.publication)),
     );
   };
-
-  useEffect(() => {
-    getArticles();
-  }, [user, getArticles]);
 
   const updateArticle = (updatedArticle: Article) => {
     setArticles((prevArticles) =>
@@ -197,34 +166,130 @@ export default function ArticleList({ session }: { session: Session | null }) {
     setSort(e.target.value);
   };
 
+  const loadMoreArticles = useCallback(async () => {
+    try {
+      setLoading(true);
+      let query = getQuery(supabase, sort, filter);
+
+      query = query.range(
+        currentPage * DEFAULT_ARTICLES_PER_PAGE,
+        (currentPage + 1) * DEFAULT_ARTICLES_PER_PAGE - 1,
+      );
+
+      let { data, error, status } = await query;
+
+      if (error && status !== 406) {
+        throw error;
+      }
+
+      if (data) {
+        const newAndOldArticles = [...articles, ...data];
+        // get unique publication names
+        const newlyAppearingPublications = Array.from(
+          new Set(data.map((article) => article.publication)),
+        );
+
+        const newPublications = Array.from(
+          new Set([...newlyAppearingPublications, ...allPublications]),
+        );
+        newPublications.sort();
+
+        // if all publications are selected, then select all new publications
+        if (allPublications.length === selectedPublications.length) {
+          setSelectedPublications(newPublications);
+        }
+
+        setArticles(
+          newAndOldArticles.filter(
+            (article) =>
+              selectedPublications.length == 0 ||
+              selectedPublications.includes(article.publication),
+          ),
+        );
+
+        setCurrentPage(currentPage + 1);
+
+        setAllPublications(newPublications);
+        setAllArticles(newAndOldArticles);
+      }
+    } catch (error) {
+      console.error("Error loading more articles:", error);
+    } finally {
+      setLoading(false);
+    }
+  }, [
+    supabase,
+    articles,
+    currentPage,
+    filter,
+    sort,
+    allPublications,
+    selectedPublications,
+  ]);
+
+  useEffect(() => {
+    if (!initialLoadDone) {
+      loadMoreArticles().then(() => setInitialLoadDone(true));
+    }
+    // no dependencies - is that same as empty?
+  });
+
+  useEffect(() => {
+    const loadMoreTrigger = document.querySelector(".load-more-trigger");
+    if (initialLoadDone) {
+      if (loadMoreTrigger) {
+        const observer = new IntersectionObserver(
+          (entries) => {
+            if (entries[0].isIntersecting) {
+              loadMoreArticles();
+            }
+          },
+          { threshold: 1.0 },
+        );
+
+        observer.observe(loadMoreTrigger);
+
+        // Clean up function
+        return () => observer.disconnect();
+      }
+    }
+  }, [loadMoreArticles, initialLoadDone]);
+
   return (
     <div>
-      <select onChange={handleFilterChange} value={filter}>
-        {filters.map((filter) => (
-          <option key={filter} value={filter}>
-            {filter}
-          </option>
-        ))}
-      </select>
-      <select onChange={handleSortChange} value={sort}>
-        {sorts.map((sort) => (
-          <option key={sort} value={sort}>
-            {sort}
-          </option>
-        ))}
-      </select>
+      <div className="flex-container">
+        <select onChange={handleFilterChange} value={filter}>
+          {filters.map((filter) => (
+            <option key={filter} value={filter}>
+              {filter}
+            </option>
+          ))}
+        </select>
+        <select onChange={handleSortChange} value={sort}>
+          {sorts.map((sort) => (
+            <option key={sort} value={sort}>
+              {sort}
+            </option>
+          ))}
+        </select>
 
-      <select
-        multiple={true}
-        value={selectedPublications}
-        onChange={handlePublicationChange}
-      >
-        {allPublications.map((option) => (
-          <option key={option} value={option}>
-            {option}
-          </option>
-        ))}
-      </select>
+        <select
+          multiple={true}
+          value={selectedPublications}
+          onChange={handlePublicationChange}
+        >
+          {allPublications.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+        <form action="/auth/signout" method="post">
+          <button className="signout button" type="submit">
+            Sign out
+          </button>
+        </form>
+      </div>
 
       <div className="articles-list">
         {articles.map((article) => (
@@ -236,12 +301,11 @@ export default function ArticleList({ session }: { session: Session | null }) {
         ))}
       </div>
       <div>
-        <form action="/auth/signout" method="post">
-          <button className="signout button block" type="submit">
-            Sign out
-          </button>
-        </form>
+        <button onClick={loadMoreArticles} disabled={loading}>
+          {loading ? "Loading..." : "Load More"}
+        </button>
       </div>
+      <div className="load-more-trigger"></div>
     </div>
   );
 }
