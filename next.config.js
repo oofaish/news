4822cb7bea2ceabe0ae37e9bc268f95b0a985f73
Ali <ali@cigari.co.uk>
2024-11-16 19:40:45 +0000

    feat(app): attempt to make a standalone app

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA(nextConfig);
